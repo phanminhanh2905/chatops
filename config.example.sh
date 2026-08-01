45eba6db3756f44bc2bae6b5cb0831635dca436d
phanminhanh2905 <admin@pma-server.site>
2026-08-01 21:45:07 +0700

    config(add): add file config example

---
+++ config.example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TOKEN="your telegram bot token"
+GITLAB_URL="your gilab url"
+GITLAB_PROJECT_ID="your gitlab project id"
+GITLAB_ACCESS_TOKEN="your gitlab access token"
+K8S_NAMESPACE="your namespace on k8s"
+
